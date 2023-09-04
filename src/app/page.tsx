52255444dbd0c37167cb50bd4cb8cefe7f28024b
Pedro Yamaguchi <hiroyamaguch@proton.me>
2023-09-04 19:24:14 -0300

    feat: change texts of home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,13 @@ import { Form } from '@/components/Form'
 export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-center space-y-6 px-5">
-      <h1 className="text-center font-sans text-xl font-medium">
-        Calcule quantas horas faltam para sair do serviço
+      <h1 className="text-center font-sans text-2xl font-bold">
+        Quanto falta?
       </h1>
+
+      <h3 className="text-center font-sans">
+        Calcule quanto tempo do expediente diário já foi realizado
+      </h3>
 
       <Form />
     </main>
