a39144bf0991157fef5b200f22029c1b8f71715e
Pedro Yamaguchi <hiroyamaguch@proton.me>
2023-09-04 19:24:39 -0300

    refact: change calc validation types

--- src/validators/calc.validator.ts
+++ src/validators/calc.validator.ts
@@ -1,15 +1,10 @@
 import * as yup from 'yup'
 
-export type CalcInputsTypes = {
-  first: string
-  second: string
-  third: string
-  fourth: string
-}
-
 export const calcValidator = yup.object().shape({
-  first: yup.string().required('Primeiro é obrigatório'),
+  first: yup.string().required('Primeira entrada é obrigatória'),
   second: yup.string(),
   third: yup.string(),
   fourth: yup.string(),
 })
+
+export type CalcInputsTypes = yup.InferType<typeof calcValidator>
