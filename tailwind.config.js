81646dd8fb3a97eb9b6d4d992953d3c394763ba2
Pedro Yamaguchi <hiroyamaguch@proton.me>
2024-02-20 23:42:23 -0300

    chore: update packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: 'var(--font-robot-slab)',
+        sans: 'var(--font-roboto-slab)',
       },
       colors: {
         input: {
